1acee1f026a49b74052c1a860906035b79848b5a
mdoutar <mdoutar7000@uca.ac.ma>
2025-12-06 19:09:25 +0000

    fix typing and shadow box

--- script.js
+++ script.js
@@ -3,22 +3,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     const typingElement = document.getElementsByClassName('typing-name');
     const text = 'Mohamed Amine Doutar';
-    let index = 0;
-    
-    typingElement[0].textContent = ''; 
-    typingElement[1].textContent = ''; 
-    function typeWriter() {
+    function typing(){
+
+      let index = 0;
+      typingElement[0].textContent = ''; 
+      typingElement[1].textContent = ''; 
+      function typeWriter() {
         if (index < text.length) {
-            typingElement[0].textContent += text.charAt(index);
-            typingElement[1].textContent += text.charAt(index);
-            index++;
-            setTimeout(typeWriter, 150); 
+          typingElement[0].textContent += text.charAt(index);
+          typingElement[1].textContent += text.charAt(index);
+          index++;
+          setTimeout(typeWriter, 150); 
         }
+      }
+      typeWriter();
     }
-    typeWriter();
+    setInterval(typing, text.length * 150);
 });
 const originalTitle = document.title;
-const awayTitle = 'coem back I miss you :>';
+const awayTitle = 'come back I miss you :>';
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
         document.title = awayTitle;
